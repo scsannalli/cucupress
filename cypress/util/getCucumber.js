97b4b15fd0cfe781370c85bd74d88e696d0de231
prasadmudedla <prasadmudedla@live.in>
2020-02-07 14:10:08 +0530

    [Added the code for fetching the cucumber files]

---
+++ cypress/util/getCucumber.js
@@ -0,0 +1,56 @@
+let request = require("supertest");
+var download = require("download-file");
+const config = require('config');
+const minimist = require('minimist');
+
+describe("API call to search the issue attachement", () => {
+
+	let attachmentLink;
+	let fileLink;
+
+	// before(async function () {
+	// 	    // fetching the command line arguments
+	// 			const args = minimist(process.argv.slice(2));
+
+	// 			// assiging  the environment globally
+	// 			const env = args.env;
+	// 			global.params = config.get(env);
+	// });
+
+	it("Get the URL for the attachement", async () => {
+		let response = await request(
+				"https://shivajira.atlassian.net/rest/api/2/issue/"
+			)
+			.get("TES-2")
+			.set("Content-Type", "application/json")
+			.auth("username", "auth")
+			.retry(3);
+		attachmentLink = await response.body.fields.attachment[0].content;
+		console.log(attachmentLink);
+	});
+
+	it("Get the data from attachement", async () => {
+		let response = await request(attachmentLink)
+			.get("")
+			.set("Content-Type", "text/plain")
+			.auth("username", "auth")
+			.retry(3);
+		fileLink = response.headers.location;
+		console.log(fileLink);
+	});
+
+	it("Dowloads the file", async () => {
+		fileName = fileLink
+			.split("&name=")
+			.slice(-1)
+			.join();
+		let options = {
+			directory: "./cypress/integration",
+			filename: fileName
+		};
+		download(fileLink, options, function (err) {
+			if (err) throw err;
+		});
+	});
+
+});
