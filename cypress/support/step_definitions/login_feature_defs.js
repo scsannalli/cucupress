229208cf1b411e161def82bd7a5faaf5d195b0cb
shiva <shiva@shivas-MacBook-Pro.local>
2020-02-24 18:24:18 +0800

    Configure Cypress to trigger backend validation|Shiva

--- cypress/support/step_definitions/login_feature_defs.js
+++ cypress/support/step_definitions/login_feature_defs.js
@@ -1,6 +1,11 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
-  
+import getPayLoad from "../../../db/mysqlactions"
 
+var envVal = false;
+beforeEach(function () {
+  cy.log('Run before every test for every spec file');
+  envVal = Cypress.env('backend')
+})
 
   Given ('I open home page', () => {
     cy.visit('localhost:3000');
@@ -14,5 +19,15 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
   Then('I see UserName Field', rating=>
   {
-     
+      if(envVal)
+      {
+        cy.log("Trigger Backend validation");
+        getPayLoad
+      } 
+
   });
+
+  afterEach(function () {
+    cy.log('Run after every test for every spec file!!!!!!');
+    cy.end  
+  })
--- db/mysqlactions.js
+++ db/mysqlactions.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'userb',
@@ -6,26 +7,33 @@ const connection = mysql.createConnection({
   database: 'sitepoint'
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
-
-connection.query('SELECT * FROM authors', (err,rows) => {
+function getPayLoad(){
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
+  connection.query('SELECT * FROM authors', (err,rows) => {
     if(err) throw err;
-  
+    cy.log("xyz");
     console.log('Data received from Db:');
     console.log(rows);
 
     rows.forEach( (row) => {
       console.log(`${row.name} lives in ${row.city}`);
     });
-  });
+  })
+  end()
+};
 
-
-
+function end()
+{
 connection.end((err) => {
     // The connection is terminated gracefully
     // Ensures all remaining queries are executed
     // Then sends a quit packet to the MySQL server.
   });
+};
+
+
+module.exports.getPayLoad = getPayLoad;
+
